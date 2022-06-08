397727a83c68e6869c91a7657921c8715ad4575e
BernardoTaveiraCarvalho <94644026+BernardoTaveiraCarvalho@users.noreply.github.com>
2022-06-08 23:23:41 +0100

    Excel and auth

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            \DB::table('users')->insert([
+                'name'      => 'Admin' ,
+                'email'=> 'eee@gmail.com',
+                'email_verified_at' =>now(),
+                'password' =>bcrypt('123456'),
+                'admin'=>1,
+                'create_at'=>now(),
+                'updated_at'=>now(),
+            ]);
+
+    }
+}
